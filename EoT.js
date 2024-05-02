2a87dc3ca9ab9c8dd9cc78588dad7ba313d41013
phintbn123 <phintbn123@gmail.com>
2024-05-02 15:35:36 +0700

    commit

---
+++ EoT.js
@@ -0,0 +1,10 @@
+function hw(){
+    window.alert("Hello world !");
+}
+function check(){
+    let pass1 = document.getElementById('pass1').value;
+    let pass2 = document.getElementById('pass2').value;
+    if (pass1 !== pass2 ){
+        window.alert("")
+    }
+}
